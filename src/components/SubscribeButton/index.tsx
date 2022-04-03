542e0d0fdb77cc452345ed989b08d9f132c352fb
ian-aquino-bianquini <ian.aquino2003@gmail.com>
2022-04-03 09:59:34 -0300

    Layout and price by stripe

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+function SubscribeButton({priceId}: SubscribeButtonProps) {
+  return (
+    <button 
+    type="button" 
+    className={styles.subscribeButton}>
+      Subscribe now!
+    </button>
+  );
+}
+
+export default SubscribeButton;
