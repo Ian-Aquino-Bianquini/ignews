458bb43b54b80899bf7fef46793f8e8cfc58c249
ian-aquino-bianquini <ian.aquino2003@gmail.com>
2022-05-06 10:07:36 -0300

    Getting list of posts in /post

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,65 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+export default function Posts({posts}: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts | IgNews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((post: Post) => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+
+  
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  const response = await prismic.get();
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.Title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
+
+  return {
+    props: {posts}
+  }
+}
